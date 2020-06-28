a8ccf609a268374efbf3df2d3a0840d139a99f9e
phophon <phophon.i@cmkl.ac.th>
2020-06-28 21:08:03 +0700

    base

---
+++ redwoodblog/web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <p>
+          HomePage
+      </p>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
